5b27694250e6f9de14eb8bf3ef4f8536c15d881f
LBPedroso <luan.pedroso@hotmail.com>
2024-09-30 21:57:03 -0300

    cria função para calculo soma e subtracao

---
+++ calculo.js
@@ -0,0 +1,15 @@
+let n1=2
+let n2=1
+
+console.log(n1+n2);
+console.log(n1-1)
+
+
+function somaSubtrai(a,b){
+    let soma = a+b;
+    let subtracao = a-b;
+    console.log("Soma:", soma);
+    console.log("Subtracao", subtracao);
+}
+
+somaSubtrai(2,1);
